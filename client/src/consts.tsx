a615bf5d216080f889279c0b8e21ca8c0e129f44
RazumeetsyaDanila <danil.kozhemyakin.99@mail.ru>
2022-10-14 17:45:59 +0300

    ...

--- client/src/consts.tsx
+++ client/src/consts.tsx
@@ -2,15 +2,20 @@
 // export const REACT_APP_API_URL: string = 'http://vps.razumeetsya.ru:5000/'
 
 // для локального домашнего
-export const REACT_APP_API_URL: string = 'http://192.168.1.107:5000/'
+// export const REACT_APP_API_URL: string = 'http://192.168.1.107:5000/'
 
 // для локального рабочего
 // export const REACT_APP_API_URL: string = 'http://192.168.0.31:5000/'
 
+// для рабочего сервера
+export const REACT_APP_API_URL: string = 'http://192.168.0.56:5000/'
+
 // для сервера react-app
 // export const REACT_APP_URL: string = 'http://vps.razumeetsya.ru:3000/'
 // export const REACT_APP_URL: string = 'http://192.168.1.107:3000/'
-export const REACT_APP_URL: string = 'http://localhost:3000/'
+// export const REACT_APP_URL: string = 'http://localhost:3000/'
+// для рабочего сервера
+export const REACT_APP_URL: string = 'sm-serv01:322/'
 
 export enum routes{
     ADMIN_ROUTE = '/admin',
