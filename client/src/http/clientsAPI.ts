ce845ac3c5b77c803aa78d4271d333b203ebd1f1
RazumeetsyaDanila <danil.kozhemyakin.99@mail.ru>
2022-11-14 22:49:34 +0300

    some changes...

--- client/src/http/clientsAPI.ts
+++ client/src/http/clientsAPI.ts
@@ -4,3 +4,8 @@ export const org_add = async (org_name: string, simed_admin_pass: string, remote
     const {data} = await $authHost.post('api/user/add_org', {org_name, simed_admin_pass, remote_access, city, comment})
     return ("Клиент добавлен!")
 }
+
+export const org_delete = async (org_id: number) => {
+    const {data} = await $authHost.post('api/admin/delete_org', {org_id})
+    return ("Уалено!")
+}
--- client/src/pages/org/OrgPage.tsx
+++ client/src/pages/org/OrgPage.tsx
@@ -1,32 +1,62 @@
-import React, {useEffect} from 'react';
-import { useParams, NavLink } from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+import { useParams, NavLink, useNavigate } from 'react-router-dom';
 import { useTypedSelector } from '../../hooks/useTypedSelector';
+import Modal from '../../components/UI/modal/Modal';
+import { org_delete } from '../../http/clientsAPI';
+import { routes } from '../../consts';
 
 const OrgPage = () => {
     const params = useParams();
-    const orgId = params.id;
+    const orgId: string = params.id!; //string (not string | undefined)
+    let orgIdNum: number = +orgId; //string to number
 
     const { clients, loading, error } = useTypedSelector(state => state.clients)
     const currentOrg = clients.find(clients => clients.org_id == orgId)
+
+    const [deleteConfirmModal, setDeleteConfirmModal] = useState(false)
+
+    const navigate = useNavigate()
 
     useEffect(() => {
         
     }, [])
 
+    const startDeleteOrg = () => {
+        setDeleteConfirmModal(true)
+    }
+
+    const deleteOrg = () => {
+        org_delete(orgIdNum)
+        setDeleteConfirmModal(false)
+        navigate(routes.ADMIN_ROUTE)
+    }
+
     if (loading) return <h1 className='centerContainer h-screen text-2xl'>Идет загрузка...</h1>
     if (error) return <h1 className='centerContainer h-screen text-2xl'>{error}</h1>
     return (
         <div className='centerContainer h-screen'>
-            <p className='text-3xl'>Организация № {orgId}</p> 
+            {/* <p className='text-3xl'>Организация № {orgId}</p> */}
             <p className='mb-[30px] text-3xl'>Название: {currentOrg.org_name}</p>
 
-            <NavLink to='/files/cert_export_CA.sokolmed.itl-service.ru.p12' target="_blank" download> скачать сертификат соколмед</NavLink>
+            {/* <NavLink to='/files/cert_export_CA.sokolmed.itl-service.ru.p12' target="_blank" download> скачать сертификат соколмед</NavLink>
             <NavLink to='/files/rdp-cdt2.bat' target="_blank" download> скачать батник rdp-cdt2.bat </NavLink>
-            <NavLink to='/files/rdp-ldc2.bat' target="_blank" download> скачать батник rdp-ldc2.bat </NavLink>
+            <NavLink to='/files/rdp-ldc2.bat' target="_blank" download> скачать батник rdp-ldc2.bat </NavLink> */}
+
+            <button className='redBtn w-[90px] h-[30px] deleteBtnPos' onClick={() => startDeleteOrg()}>Удалить</button>
 
             <NavLink to='/admin' className='btn w-[90px] h-[30px] backBtnPos'>
                 ← назад
             </NavLink>
+
+            <Modal visible={deleteConfirmModal} setVisible={setDeleteConfirmModal}>
+                <div className='flex flex-col items-center'>
+                    <p className='mb-[20px] text-[20px]'>Удалить?</p>
+                    <div className='flex justify-between w-[200px]'>
+                        <button className='btn w-[60px] h-[30px]' onClick={deleteOrg}>Да</button>
+                        <button className='btn w-[60px] h-[30px]' onClick={() => setDeleteConfirmModal(false)}>Нет</button>
+                    </div>
+                </div>
+            </Modal>
         </div>
     );
 };
